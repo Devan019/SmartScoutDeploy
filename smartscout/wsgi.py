e0981623047482780bec41e562aecc96de485082
Devan019 <devanchauhan012@gmail.com>
2025-04-07 22:18:59 +0530

    update wsgi

--- smartscout/wsgi.py
+++ smartscout/wsgi.py
@@ -6,6 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
+import collections
+import collections.abc
+collections.Sequence = collections.abc.Sequence
 import os
 
 from django.core.wsgi import get_wsgi_application
